2b75c13adb38c6e190b0cf85859618b6c6b1b756
MOKTADIR <moktadir@MOKTADIRs-iMac-Pro.local>
2023-04-16 04:51:40 +0600

    annotating objects

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from django.db.models.aggregates import Count
-from store.models import Product
+from django.db.models import Value, F, ExpressionWrapper, DecimalField
+from django.db.models.functions import Concat
+from store.models import Product, Customer
 
 
 # Create your views here.
 
 def hello(request):
-    # we can have any summary like total count, max item, max price we use aggregate.
-    # we can filter them too
-    result = Product.objects.filter(collection__id=1).aggregate(count=Count('id'))
-    return render(request, 'hello.html', {'name': 'Moktadir', 'result': result})
+    # we can add new attribute to query objects with annotations
+    custom_id = ExpressionWrapper(
+        F("id") * 0.8, output_field=DecimalField())
+    query_set = Customer.objects.annotate(is_new=Value(True), new_id=F('id') + 1,
+                                          full_name=Concat("first_name", Value(" "), "last_name"),
+                                          custom_id=custom_id
+                                          )
+    return render(request, 'hello.html', {'name': 'Moktadir', 'result': list(query_set)})

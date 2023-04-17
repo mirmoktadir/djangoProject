77bacac552845d17e3fcf5dcab4386c005c6786e
MOKTADIR <moktadir@MOKTADIRs-iMac-Pro.local>
2023-04-18 02:39:40 +0600

    query from generic models

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render
-from django.db.models import Value, F, ExpressionWrapper, DecimalField
-from django.db.models.functions import Concat
-from store.models import Product, Customer
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product
+from tags.models import TaggedItem
 
 
 # Create your views here.
 
 def hello(request):
-    # we can add new attribute to query objects with annotations
-    custom_id = ExpressionWrapper(
-        F("id") * 0.8, output_field=DecimalField())
-    query_set = Customer.objects.annotate(is_new=Value(True), new_id=F('id') + 1,
-                                          full_name=Concat("first_name", Value(" "), "last_name"),
-                                          custom_id=custom_id
-                                          )
-    return render(request, 'hello.html', {'name': 'Moktadir', 'result': list(query_set)})
+    queryset = TaggedItem.objects.get_tags_for(Product, 1)
+    return render(request, 'hello.html', {'name': 'Moktadir', 'result': list(queryset)})

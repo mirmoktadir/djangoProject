af0eab63186ada1da106f1b8234ec73ce56c6145
MOKTADIR <moktadir@MOKTADIRs-iMac-Pro.local>
2023-04-18 04:47:51 +0600

    CRUDE objects

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-from django.contrib.contenttypes.models import ContentType
-from store.models import Product
-from tags.models import TaggedItem
+from store.models import Collection, Product
 
 
 # Create your views here.
 
 def hello(request):
-    queryset = TaggedItem.objects.get_tags_for(Product, 1)
-    return render(request, 'hello.html', {'name': 'Moktadir', 'result': list(queryset)})
+    # 1.>adding objects
+    # collection = Collection()
+    # collection.title="Hello"
+    # collection.featured_product=Product(pk=1)
+    # collection.save()
+    # 2.>updating object
+    # Collection.objects.filter(pk=11).update(featured_product=None)
+    # 3.> deleting objects
+    # single object delete >
+    collection = Collection(pk=11)
+    collection.delete()
+    # multiple object delete >
+    # Collection.objects.filter(id__gt=5).delete()
+    return render(request, 'hello.html', {'name': 'Moktadir'})
